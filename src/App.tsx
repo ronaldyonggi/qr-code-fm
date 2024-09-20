173a4ed30e8d93f0e38000ca482573f4b0c402e7
Ronald Yonggi <ronaldyonggi@gmail.com>
2024-09-20 17:12:00 +0800

    define App

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
 import './App.css'
+import Card from './components/Card';
 
 function App() {
-
   return (
-    <>
-    </>
-  )
+    <div>
+      <Card />
+    </div>
+  );
 }
 
-export default App
+export default App;
