e18076e54b5424e6cf18ec3e0caf61e39db3fe05
Ronald Yonggi <ronaldyonggi@gmail.com>
2024-09-23 14:06:01 +0800

    refactor App files to main.tsx and index.css

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,14 @@
 import { createRoot } from 'react-dom/client';
-import App from './App.tsx';
 import './reset.css';
 import './index.css';
+import Card from './components/Card';
+
+export default function App() {
+  return (
+    <main>
+      <Card />
+    </main>
+  );
+}
 
 createRoot(document.getElementById('root')!).render(<App />);
